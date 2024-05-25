35f4ed164b8dc0383a8c1548925688034cf72109
MarksmanX <mkeph561@gmail.com>
2024-05-25 16:32:57 -0400

    Finished

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("Let's get this party started!");
+const $gifArea = $("#gif-area");
+const $searchInput = $("#search");
+
+function addGif(res) {
+    let numResults = res.data.length;
+    if (numResults) {
+        let randomIdx = Math.floor(Math.random() * numResults);
+        let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
+        let $newGif = $("<img>", {
+        src: res.data[randomIdx].images.original.url,
+        class: "w-100"
+        });
+        $newCol.append($newGif);
+        $gifArea.append($newCol);
+    }
+};   
+
+$("form").on("submit", async function(e) {
+    e.preventDefault();
+
+    let searchTerm = $searchInput.val();
+    $searchInput.val("");
+
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        params: {
+          q: searchTerm,
+          api_key: "U2Clshm65waA5FS6SwcAbQCRgI9KGjSx"
+        }
+    });
+    addGif(response.data);
+});
+
+$("#remove").on("click", function() {
+    $gifArea.empty();
+});
